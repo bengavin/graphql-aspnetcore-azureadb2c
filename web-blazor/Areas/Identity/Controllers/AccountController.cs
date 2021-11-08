3492937a67363617aa0570385283a9dfb7030cbc
Ben Gavin <ben@virtual-olympus.com>
2021-11-08 16:27:31 -0600

    UPDATE:  Change sign-out behavior to redirect to home page

---
+++ web-blazor/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace StarWars.UI.Blazor.Areas.Identity.Controllers
+{
+    /// <summary>
+    /// This 'override' was copied from the Microsoft Identity Web UI source
+    /// to change the behavior to redirect to the home page once logged out
+    /// </summary>
+    [AllowAnonymous]
+    [Area("Identity")]
+    [Route("[area]/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Handles the user sign-out.
+        /// </summary>
+        /// <param name="scheme">Authentication scheme.</param>
+        /// <returns>Sign out result.</returns>
+        [HttpGet("{scheme?}")]
+        public IActionResult SignOut(
+            [FromRoute] string scheme)
+        {
+            if (AppServicesAuthenticationInformation.IsAppServicesAadAuthenticationEnabled)
+            {
+                return LocalRedirect(AppServicesAuthenticationInformation.LogoutUrl);
+            }
+            else
+            {
+                scheme ??= OpenIdConnectDefaults.AuthenticationScheme;
+                var callbackUrl = Url.Page("/", pageHandler: null, values: null, protocol: Request.Scheme);
+                return SignOut(
+                     new AuthenticationProperties
+                     {
+                         RedirectUri = callbackUrl,
+                     },
+                     CookieAuthenticationDefaults.AuthenticationScheme,
+                     scheme);
+            }
+        }
+    }
+}
+
